902188842f3c739f747d0ca57a96dfb1ff667c87
JaganPothina <124559539+JaganPothina@users.noreply.github.com>
2023-04-20 17:49:02 +0530

    Update terraform-jenkins-server-files.tf

--- terraform-jenkins-server-files.tf
+++ terraform-jenkins-server-files.tf
@@ -4,7 +4,7 @@ provider aws {
 resource "aws_instance" "jenkins-medicure" {
   ami           = "ami-007855ac798b5175e" 
   instance_type = "t2.medium"
-  key_name = "Projectkey"
+  key_name = "jenkinskey"
   vpc_security_group_ids= ["sg-0c7aae9017fc5106b"]
   
    tags = {
