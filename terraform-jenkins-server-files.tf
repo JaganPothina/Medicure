2591eb0dcf3170e7c4dd35cf733b0b08e331b1bc
JaganPothina <124559539+JaganPothina@users.noreply.github.com>
2023-04-20 12:33:00 +0530

    Update terraform-jenkins-server-files.tf

--- terraform-jenkins-server-files.tf
+++ terraform-jenkins-server-files.tf
@@ -1,19 +1,19 @@
 provider aws {
     region = "us-east-1"
 }
-resource "aws_instance" "jenkins-server" {
+resource "aws_instance" "jenkins-medicure" {
   ami           = "ami-007855ac798b5175e" 
   instance_type = "t2.medium"
   key_name = "Projectkey"
   vpc_security_group_ids= ["sg-0c7aae9017fc5106b"]
-
+  
    tags = {
-    Name = "Jenkins-server"
+    Name = "Jenkins-medicure"
   }
 }
 
 output "Jenkins-server_public_ip" {
 
-  value = aws_instance.jenkins-server.public_ip
+  value = aws_instance.jenkins-medicure.public_ip
   
 }
